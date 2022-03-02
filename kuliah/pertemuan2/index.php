69aad19294ecfa5d9577478244aa6a4486d939e0
farraskhair <farras.213040105@mail.unpas.ac.id>
2022-03-02 19:45:46 +0700

    mengerjakan latihan pertemuan 2

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,92 @@
+<?php
+//inikomentar
+/*ini komentar
+*/
+
+
+// pertemuan2 - php dasar
+
+
+// sintaks php
+// echo "farras khair";
+// print "farrask";
+// kalo pake var_dump keliatan string nya
+// var_dump("farraskhair");
+// echo "true"; bakalan keluar 1
+// echo "false"; ga bakalan ada/muncul
+
+// $nama = "farras";
+// operator
+// aritmatika
+// +-*/%
+// echo 1 + 1;
+// $x = 10;
+// $y = 20;
+// echo $x + $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "farras";
+// $nama_belakang = "khair";
+// echo $nama_depan . " " . $nama_belakang
+
+// operator assigment
+// =, +=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "farras";
+// $nama.= " ";
+// $nama.= "khair";
+// echo $nama;
+
+// perbandingan
+// <, >, <=, >=, ==
+// var_dump(1 == "1");
+
+//  identitas (buat mengecek sebuah nilai)
+// ===, !==
+// var_dump(1 === "1");
+
+// logika
+// &&, ||, !
+// $x = 10;
+// var_dump($x < 20 && $x % 2== 0 );
+?>
+<!--
+//  penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+/*1.<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>halo, selamat datang <?php echo $nama;?></h1>
+</body>
+</html>
+ */ -->
+
+<!-- <!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    /*<?php 
+    echo "<h1>hallo, selamat datang farras</h1>"
+    ?>*/
+</body>
+</html>
+-->
+
+ 
+
+
+
