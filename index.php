d7f20d370c6e674a73876fb6ee7a395868ab82b1
faras2 <96606273+faras2@users.noreply.github.com>
2022-06-09 23:07:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,262 @@
+<?php
+
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>HOTTEC</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="assets/font/font-awesome.min.css"
+    />
+    <link rel="stylesheet" type="text/css" href="assets/font/font.css" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="assets/css/bootstrap.min.css"
+      media="screen"
+    />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="assets/css/style.css"
+      media="screen"
+    />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="assets/css/responsive.css"
+      media="screen"
+    />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="assets/css/jquery.bxslider.css"
+      media="screen"
+    />
+  </head>
+  <body>
+    <div class="body_wrapper">
+      <div class="center">
+        <div class="header_area">
+          <div class="logo floatleft">
+            <a href="sites/login.php"><img src="images/logo2.png" alt="" /></a>
+          </div>
+          <div class="top_menu floatleft">
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact us</a></li>
+              <li><a href="#">Subscribe</a></li>
+              <li><a href="sites/login.php">Login</a></li>
+            </ul>
+          </div>
+          <div class="social_plus_search floatright">
+            <div class="social">
+              <ul>
+                <li><a href="#" class="twitter"></a></li>
+                <li><a href="#" class="facebook"></a></li>
+                <li><a href="#" class="feed"></a></li>
+              </ul>
+            </div>
+            <div class="search">
+              <form action="#" method="post" id="search_form">
+                <input type="text" value="Search news" id="s" />
+                <input type="submit" id="searchform" value="search" />
+                <input type="hidden" value="post" name="post_type" />
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="main_menu_area">
+          <ul id="nav">
+            <li>
+              <a href="#">E-news</a>
+              <ul>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+                <li>
+                  <a href="#">Single item</a>
+                  <ul>
+                    <li><a href="#">Single item</a></li>
+                    <li><a href="#">Single item</a></li>
+                    <li><a href="#">Single item</a></li>
+                    <li><a href="#">Single item</a></li>
+                    <li><a href="#">Single item</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+              </ul>
+            </li>
+            <li><a href="#">E-sports</a></li>
+            <li>
+              <a href="#">Space Program</a>
+              <ul>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Cryptocurrency</a></li>
+            <li>
+              <a href="#">Healty Tech</a>
+              <ul>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+                <li>
+                  <a href="#">Single item</a>
+                  <ul>
+                    <li><a href="#">Single item</a></li>
+                    <li><a href="#">Single item</a></li>
+                    <li><a href="#">Single item</a></li>
+                    <li><a href="#">Single item</a></li>
+                    <li><a href="#">Single item</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Digital Literacy</a></li>
+            <li><a href="#">culture</a></li>
+            <li>
+              <a href="#">Books</a>
+              <ul>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+                <li><a href="#">Single item</a></li>
+              </ul>
+            </li>
+            <li><a href="#">classifieds</a></li>
+            <li><a href="#">blogs</a></li>
+          </ul>
+        </div>
+        <div class="slider_area">
+          <div class="slider">
+            <ul class="bxslider">
+              <li>
+                <img src="images/futureedit.jpg" alt="" title="Slider caption text" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="content_area">
+          
+              <div class="single_left_coloum_wrapper single_cat_left">
+                <h2 class="title">tech news</h2>
+                <a class="more" href="#">more</a>
+                <div class="single_cat_left_content floatleft">
+                  <h3>
+                    Lorem ipsum dolor sit amet conse ctetur adipiscing elit
+                  </h3>
+                  <p>
+                    Nulla quis lorem neque, mattis venenatis lectus. In interdum
+                    ullamcorper dolor ...interdum
+                  </p>
+                  <p class="single_cat_left_content_meta">
+                    by <span>John Doe</span> | 29 comments
+                  </p>
+                </div>
+                <div class="single_cat_left_content floatleft">
+                  <h3>
+                    Lorem ipsum dolor sit amet conse ctetur adipiscing elit
+                  </h3>
+                  <p>
+                    Nulla quis lorem neque, mattis venenatis lectus. In interdum
+                    ullamcorper dolor ...interdum
+                  </p>
+                  <p class="single_cat_left_content_meta">
+                    by <span>John Doe</span> | 29 comments
+                  </p>
+                </div>
+                <div class="single_cat_left_content floatleft">
+                  <h3>
+                    Lorem ipsum dolor sit amet conse ctetur adipiscing elit
+                  </h3>
+                  <p>
+                    Nulla quis lorem neque, mattis venenatis lectus. In interdum
+                    ullamcorper dolor ...interdum
+                  </p>
+                  <p class="single_cat_left_content_meta">
+                    by <span>John Doe</span> | 29 comments
+                  </p>
+                </div>
+                <div class="single_cat_left_content floatleft">
+                  <h3>
+                    Lorem ipsum dolor sit amet conse ctetur adipiscing elit
+                  </h3>
+                  <p>
+                    Nulla quis lorem neque, mattis venenatis lectus. In interdum
+                    ullamcorper dolor ...interdum
+                  </p>
+                  <p class="single_cat_left_content_meta">
+                    by <span>John Doe</span> | 29 comments
+                  </p>
+                </div>
+              </div>
+            </div>
+          
+          <div class="sidebar floatright">
+            <div class="single_sidebar">
+              <div class="news-letter">
+                <h2>Sign Up for Newsletter</h2>
+                <p>Sign up to receive our free newsletters!</p>
+                <form action="#" method="post">
+                  <input type="text" value="Name" id="name" />
+                  <input type="text" value="Email Address" id="email" />
+                  <input type="submit" value="SUBMIT" id="form-submit" />
+                </form>
+                <p class="news-letter-privacy">
+                  We do not spam. We value your privacy!
+                </p>
+              </div>
+            </div>  
+               
+              </div>
+            </div>
+           
+        <div class="footer_bottom_area">
+          <div class="footer_menu">
+            <ul id="f_menu">
+              <li><a href="#">world news</a></li>
+              <li><a href="#">sports</a></li>
+              <li><a href="#">tech</a></li>
+              <li><a href="#">business</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">entertainment</a></li>
+              <li><a href="#">culture</a></li>
+              <li><a href="#">Books</a></li>
+              <li><a href="#">classifieds</a></li>
+              <li><a href="#">blogs</a></li>
+            </ul>
+          </div>
+          <div class="copyright_text">
+            <p>
+              Copyright &copy; 2045 The News Reporter Inc. All rights reserved |
+              Design by
+              <a
+                target="_blank"
+                rel="nofollow"
+                href="http://www.graphicsfuel.com/2045/10/wp-magazine-theme-template-psd/"
+                >Rafi MD</a
+              >
+            </p>
+            <p>
+              Trade marks and images used in the design are the copyright of
+              their respective owners and are used for demo purposes only.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </script>
+  </body>
+</html>
